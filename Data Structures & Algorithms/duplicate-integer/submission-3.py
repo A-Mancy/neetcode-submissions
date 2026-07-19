520ee61de3df8c5e96def0e32120df5a4ad4f1a1
Ali Elmancy <59843187+A-Mancy@users.noreply.github.com>
2026-07-18 22:14:12 -0400

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        if nums == [] or len(nums) == 1:
+            return False
+        first = nums[0]
+        last = nums[len(nums)-1]
+        if first == last:
+            return True
+        for num in nums[1:len(nums)-1]:
+            if num == first:
+                return True
+
+            if num == last: 
+                return True
+        return False
+        
